1646da355dc37f7c1e197f405d17faed1ad618af
chadi abou karam <chadiaboukaram@chadis-mbp.home>
2017-07-23 20:47:48 +0200

    Refactor du fetchWeatherTask avant de le transférer dans le service.

--- app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
+++ app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
@@ -36,23 +36,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Vector;
 
 public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
 
     private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-
+    private boolean DEBUG = true;
     private final Context mContext;
 
-    public FetchWeatherTask(Context context) {
+    public FetchWeatherTask(Context context)
+    {
         mContext = context;
     }
-
-    private boolean DEBUG = true;
-
-
 
     /**
      * Helper method to handle insertion of a new location in the weather database.
@@ -63,7 +60,8 @@ public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
      * @param lon the longitude of the city
      * @return the row ID of the added location.
      */
-    long addLocation(String locationSetting, String cityName, double lat, double lon) {
+    long addLocation(String locationSetting, String cityName, double lat, double lon)
+    {
         // Students: First, check if the location with this city name exists in the db
         long locationId;
 
@@ -107,15 +105,15 @@ public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
 
 
     /**
+     * Helper method :
      * Take the String representing the complete forecast in JSON Format and
      * pull out the data we need to construct the Strings needed for the wireframes.
      *
      * Fortunately parsing is easy:  constructor takes the JSON string and converts it
      * into an Object hierarchy for us.
      */
-    private void getWeatherDataFromJson(String forecastJsonStr, String locationSetting) throws JSONException
+    private void insertWeatherDataFromJsonIntoDB(String forecastJsonStr, String locationSetting)
     {
-
         // Now we have a String representing the complete forecast in JSON Format.
         // Fortunately parsing is easy:  constructor takes the JSON string and converts it
         // into an Object hierarchy for us.
@@ -148,7 +146,8 @@ public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
         final String OWM_DESCRIPTION = "main";
         final String OWM_WEATHER_ID = "id";
 
-        try {
+        try
+        {
             JSONObject forecastJson = new JSONObject(forecastJsonStr);
             JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
 
@@ -247,111 +246,131 @@ public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
             Log.d(LOG_TAG, "FetchWeatherTask Complete. " + cVVector.size() + " Inserted");
 
 
-        } catch (JSONException e) {
+        } catch (JSONException e)
+        {
             Log.e(LOG_TAG, e.getMessage(), e);
             e.printStackTrace();
         }
     }
 
-    @Override
-    protected Void doInBackground(String... params) {
 
-        // If there's no zip code, there's nothing to look up.  Verify size of params.
-        if (params.length == 0) {
-            return null;
-        }
-        String locationQuery = params[0];
+    private URL buildURL(String city_id)
+    {
+        URL url = null;
 
-        // These two need to be declared outside the try/catch
-        // so that they can be closed in the finally block.
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-
-        // Will contain the raw JSON response as a string.
-        String forecastJsonStr = null;
-
-//        String format = "json";
-//        String units = "metric";
-//        int numDays = 14;
-
-        String city_id = params[0];
         String format = "json";
         String units = "metric";
         int numDays = 10;
-
         String api_key = BuildConfig.OPEN_WEATHER_MAP_API_KEY;
 
+        // Construct the URL for the OpenWeatherMap query
+        // Possible parameters are avaiable at OWM's forecast API page, at
+        // http://openweathermap.org/API#forecast
+        final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+        final String ID_PARAM = "q";
+        final String FORMAT_PARAM = "mode";
+        final String UNITS_PARAM = "units";
+        final String DAYS_PARAM = "cnt";
+        final String APIKEY_PARAM = "appid";
+
+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(ID_PARAM, city_id)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .appendQueryParameter(APIKEY_PARAM, api_key)
+                .build();
+
         try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
-            final String ID_PARAM = "q";
-            final String FORMAT_PARAM = "mode";
-            final String UNITS_PARAM = "units";
-            final String DAYS_PARAM = "cnt";
-            final String APIKEY_PARAM = "appid";
-
-            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                    .appendQueryParameter(ID_PARAM, city_id)
-                    .appendQueryParameter(FORMAT_PARAM, format)
-                    .appendQueryParameter(UNITS_PARAM, units)
-                    .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                    .appendQueryParameter(APIKEY_PARAM, api_key)
-                    .build();
-
-
-            URL url = new URL(builtUri.toString());
-
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            // Read the input stream into a String
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                // Nothing to do.
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
-                return null;
-            }
-            forecastJsonStr = buffer.toString();
-            getWeatherDataFromJson(forecastJsonStr, locationQuery);
-        } catch (IOException e) {
-            Log.e(LOG_TAG, e.getMessage(), e);
-            /*
-            If the code didn't successfully get the weather data, there's no point in attemping
-            to parse it.
-            */
-        } catch (JSONException e) {
-            Log.e(LOG_TAG, e.getMessage(), e);
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
             e.printStackTrace();
+            url = null;
         }
-        finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
+        return url;
+    }
+
+
+    private String downloadWeatherData(String city_id)
+    {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String forecastJsonStr = null;
+        URL url = buildURL(city_id);
+
+        if (url != null)
+        {
+            try
+            {
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+
             }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
+            catch (IOException e)
+            {
+                Log.e(LOG_TAG, e.getMessage(), e);
+            /* If the code didn't successfully get the weather data, there's no point in attemping to parse it.*/
+                return null;
+            }
+            finally
+            {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
                 }
             }
+        }
+        return forecastJsonStr;
+    }
+
+
+
+    @Override
+    protected Void doInBackground(String... params)
+    {
+
+        // If there's no zip code, there's nothing to look up.  Verify size of params.
+        if (params.length == 0) { return null; }
+        String city_id = params[0];
+
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+
+
+        // Will contain the raw JSON response as a string.
+        String forecastJsonStr = null;
+        forecastJsonStr = downloadWeatherData(city_id);
+
+        if (forecastJsonStr != null)
+        {
+            insertWeatherDataFromJsonIntoDB(forecastJsonStr, city_id);
         }
         return null;
     }
--- app/src/main/java/com/example/android/sunshine/app/services/SunshineService.java
+++ app/src/main/java/com/example/android/sunshine/app/services/SunshineService.java
@@ -14,16 +14,8 @@ import android.util.Log;
  */
 public class SunshineService extends IntentService {
 
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "com.example.android.sunshine.app.services.action.FOO";
-    private static final String ACTION_BAZ = "com.example.android.sunshine.app.services.action.BAZ";
-
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "com.example.android.sunshine.app.services.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.example.android.sunshine.app.services.extra.PARAM2";
-
     private static final String TAG="BADABOOOO";
+
 
     public SunshineService() {
         super("SunshineService");
@@ -38,61 +30,15 @@ public class SunshineService extends IntentService {
     // TODO: Customize helper method
     public static void startActionFoo(Context context, String param1, String param2) {
         Intent intent = new Intent(context, SunshineService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, SunshineService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
         context.startService(intent);
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
-            }
 
-
-        }
-    }
-
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
         // TODO: Handle action Foo
-        Log.v(TAG, param1);
-        Log.v(TAG, param2);
+        Log.v(TAG, TAG);
+
     }
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 }
